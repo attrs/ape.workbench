1733a7a33d7d25d0f2d3e37a7d3be04f682b711d
joje <joje.attrs@gmail.com>
2015-02-07 07:43:24 +0900

    ...

--- src/activator.js
+++ src/activator.js
@@ -7,47 +7,50 @@ module.exports = {
 	start: function(ctx) {
 		var http = ctx.require('plexi.http');
 		
+		var create = function(id, options) {
+			if( !id || typeof id !== 'string' ) return console.error('workbench id must be a string', id);
+			if( workbenches[id] ) return console.error('already exists workbench id', id);
+			
+			if( typeof options === 'string' ) options = {docbase:options};
+		
+			var workbench = new Workbench(options);
+			var router = http.create(id).docbase(options.docbase);
+			
+			router.static('/', path.resolve(__dirname, '../www/'));
+			router.static('/index.html', path.resolve(__dirname, '../www/index.html'));
+			router.static('/workbench.html', path.resolve(__dirname, '../www/index.html'));
+			router.get('/workbench.json', function(req, res, next) {
+				res.send(workbench);
+			});
+			
+			workbench.router = router;
+			
+			var mount = options.mount;
+			if( mount && mount.path ) {
+				if( mount.all ) {
+					http.mountToAll(mount.path, router);
+				} else if( mount.server ) {
+					var server = http.server(mount.server);
+					if( server ) server.mount(mount.path, router);
+					else console.error('[workbench] not found server', mount.server);
+				} else {
+					http.mount(mount.path, router);
+				}
+			}
+			
+			return workbenches[id] = workbench;
+		};
+		
+		var config = ctx.preference;
+		for(var k in config.workbenches) {
+			create(k, config.workbenches[k]);
+		}
+		
 		return {
 			Workbench: Workbench,
-			create: function(id, options) {
-				if( !id || typeof id !== 'string' ) return console.error('workbench id must be a string', id);
-				if( workbenches[id] ) return console.error('already exists workbench id', id);
-				
-				if( typeof options === 'string' ) options = {docbase:options};
-			
-				var workbench = new Workbench(options);
-				var bucket = http.create(id).docbase(options.docbase);
-				
-				if( typeof options.pagename === 'string' && options.pagename !== 'index.html' && !~options.pagename.indexOf('/') ) {
-					bucket.static('/' + options.pagename, path.resolve(__dirname, '../www/index.html'));
-				} else {
-					bucket.static('/', path.resolve(__dirname, '../www/index.html'));
-					bucket.static('/index.html', path.resolve(__dirname, '../www/index.html'));
-				}
-				bucket.static('/workbench.css', path.resolve(__dirname, '../www/workbench.css'));
-				bucket.static('/workbench.js', path.resolve(__dirname, '../www/workbench.js'));
-				bucket.static('/types/', path.resolve(__dirname, '../www/types'));
-				bucket.get('/workbench.json', function(req, res, next) {
-					res.send(workbench);
-				});
-				//bucket.static('/', path.resolve(__dirname, options.docbase));
-				
-				workbench.bucket = bucket;
-				
-				var mount = options.mount;
-				if( mount && mount.path ) {
-					if( mount.server ) {
-						var server = http.get(options.server);
-						server.mount(mount.path, bucket);
-					} else {
-						http.mount(mount.path, bucket);
-					}
-				}
-				
-				return workbenches[id] = workbench;
-			},
+			create: create,
 			get: function(id) {
-				return workbenches[id || 'default'];
+				return workbenches[id];
 			},
 			all: function() {
 				var arr = [];
